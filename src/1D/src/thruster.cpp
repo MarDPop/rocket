1e2f5fe13f06a7a773147599ffefafd607e631c5
Marius <marius.popescu20@gmail.com>
2022-08-23 21:29:43 -0600

    added load

--- src/1D/src/thruster.cpp
+++ src/1D/src/thruster.cpp
@@ -102,6 +102,24 @@ void SolidThruster::save(std::string fn) {
     }
 }
 
+void SolidThruster::load(std::string fn) {
+    std::ifstream myfile(fn);
+    this->times.clear();
+    this->mass_rates.clear();
+    this->p_exit.clear();
+    this->v_exit.clear();
+    if(myfile.is_open()){
+        for( std::string line; getline( myfile, line ); ){
+            auto row = util::split(line);
+            times.push_back(std::stod(row[0]));
+            mass_rates.push_back(std::stod(row[1]));
+            p_exit.push_back(std::stod(row[2]));
+            v_exit.push_back(std::stod(row[3]));
+        }
+        myfile.close();
+    }
+}
+
 SugarThruster::SugarThruster() {}
 
 SugarThruster::~SugarThruster() {}
