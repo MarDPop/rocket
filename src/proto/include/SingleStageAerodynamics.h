2ad495efa4ec39f5cf9a697762f0d727ebcaddfb
Marius <marius.popescu20@gmail.com>
2023-02-15 10:58:57 -0700

    refactored aero: todo: fix mass from computed thruster

--- src/proto/include/SingleStageAerodynamics.h
+++ src/proto/include/SingleStageAerodynamics.h
@@ -31,7 +31,10 @@ struct AeroValues {
 };
 
 
-class SingleStageAerodynamics {
+class SingleStageAerodynamics
+{
+
+    static constexpr double AOA_THRESHOLD = 1e-6;
 
     double CD0;
 
@@ -49,6 +52,10 @@ class SingleStageAerodynamics {
 
     double stall_angle;
 
+    double sin_stall_angle;
+
+    double sin_zero_lift_angle;
+
     double CL_max;
 
     double constant_term;
@@ -58,6 +65,19 @@ class SingleStageAerodynamics {
     SingleStageRocket& rocket;
 
     void compute_aero_values();
+
+    double get_parasitic_drag_from_mach(double mach);
+
+    double get_angle_of_attack();
+
+    struct aero_coef
+    {
+        double CL = 0.0;
+        double CD = 0.0;;
+        double CM = 0.0;;
+    };
+
+    aero_coef get_aero_coef(double sAoA);
 
 public:
 
--- src/proto/src/SingleStageAerodynamics.cpp
+++ src/proto/src/SingleStageAerodynamics.cpp
@@ -2,11 +2,10 @@
 
 #include "../include/SingleStageRocket.h"
 
-SingleStageAerodynamics::SingleStageAerodynamics(SingleStageRocket& r) : rocket(r) {
+SingleStageAerodynamics::SingleStageAerodynamics(SingleStageRocket& r) : rocket(r) {}
 
-}
-
-void SingleStageAerodynamics::set_coef(double* coef) {
+void SingleStageAerodynamics::set_coef(double* coef)
+{
 
     this->CD0 = coef[0]*coef[5];
     this->CL_alpha = coef[1]*coef[5];
@@ -16,13 +15,16 @@ void SingleStageAerodynamics::set_coef(double* coef) {
     this->ref_area = coef[5];
     this->ref_length = coef[6];
     this->stall_angle = coef[7];
+    this->sin_stall_angle = sin(this->stall_angle);
+    this->sin_zero_lift_angle = 2*this->sin_stall_angle;
     this->CL_max = this->CL_alpha*this->stall_angle;
     this->constant_term = 1.0/sin(this->stall_angle);
     this->CM_max = this->CM_alpha*2*this->stall_angle;
 
 }
 
-void SingleStageAerodynamics::compute_aero_values() {
+void SingleStageAerodynamics::compute_aero_values()
+{
     Vector air_velocity = this->rocket.state.velocity - this->rocket.altitude_table.wind.wind;
 
     this->aero_values.airspeed = air_velocity.norm();
@@ -38,72 +40,110 @@ void SingleStageAerodynamics::compute_aero_values() {
     this->aero_values.dynamic_pressure = this->rocket.altitude_table.values->pressure*(tmp*tmp*tmp*sqrt(tmp) - 1.0);
 }
 
-void SingleStageAerodynamics::update() {
-
-    this->compute_aero_values();
-
-    if(this->aero_values.airspeed < 1e-2)
+double SingleStageAerodynamics::get_parasitic_drag_from_mach(double mach)
+{
+    if (this->aero_values.mach < 0.5)
     {
-        this->force.zero();
-        this->moment.zero();
-        return;
+        return this->CD0;
     }
-
-    double CD0_compressible;
-
-    if (this->aero_values.mach < 0.5) {
-        CD0_compressible = this->CD0;
-    } else {
-        if(this->aero_values.mach > 1) {
-            CD0_compressible = this->CD0 + this->CD0/this->aero_values.mach;
-        } else {
-            CD0_compressible = this->CD0*(this->aero_values.mach - 0.5)*2.0;
-        }
-    }
-
-    double proj = rocket.state.CS.axis.z.dot(this->aero_values.unit_v_air);
-
-    this->moment = rocket.state.angular_velocity*(this->CM_alpha_dot*this->aero_values.dynamic_pressure);
-
-    if (proj > 0.999999) {
-        this->force = this->aero_values.unit_v_air*(-CD0_compressible*this->aero_values.dynamic_pressure);
-        return;
-    }
-
-    double AoA = (proj > 0.9) ? sqrt(2 - 2*proj) : acos(proj);
-
-    Vector arm = this->aero_values.unit_v_air.cross(rocket.state.CS.axis.z);
-
-    this->moment -= arm*(this->CM_alpha*this->aero_values.dynamic_pressure);
-
-    // could not use AoA but instead sAoA = arm.norm()
-
-    double CL,CD;
-    if(AoA > this->stall_angle)
+    else
     {
-        CD = CD0_compressible + this->K*this->CL_max*this->CL_max*arm.norm()*this->constant_term; // norm of arm is equal to sin of angle which is approx lift curve
-        if(AoA > 2*this->stall_angle)
+        if(this->aero_values.mach > 1)
         {
-            this->force = this->aero_values.unit_v_air*(-CD*this->aero_values.dynamic_pressure);
-            return;
+            return this->CD0 + this->CD0/this->aero_values.mach;
         }
         else
         {
-            double frac = 1.0/(5*(AoA - this->stall_angle) + 1.0);
-            CL = frac*this->CL_max;
+            return this->CD0*(this->aero_values.mach - 0.5)*2.0;
+        }
+    }
+}
+
+double SingleStageAerodynamics::get_angle_of_attack()
+{
+    double proj = std::min(1.0, rocket.state.CS.axis.z.dot(this->aero_values.unit_v_air));
+
+    return (proj > 0.9) ? sqrt(2.0 - 2*proj) : acos(proj);
+}
+
+SingleStageAerodynamics::aero_coef SingleStageAerodynamics::get_aero_coef(double sAoA)
+{
+    SingleStageAerodynamics::aero_coef output;
+
+    output.CD = this->get_parasitic_drag_from_mach(this->aero_values.mach);
+
+    // for small angles of attack only compute parasitic drag (no lift or moments)
+    if (sAoA < AOA_THRESHOLD)
+    {
+        return output;
+    }
+
+    output.CM = sAoA*this->CM_alpha;
+
+    if(sAoA > this->sin_stall_angle)
+    {
+        output.CD += this->K*this->CL_max*this->CL_max*sAoA*this->constant_term; // norm of arm is equal to sin of angle which is approx lift curve
+        if(sAoA > this->sin_zero_lift_angle)
+        {
+            return output;
+        }
+        else
+        {
+            double frac = 1.0/(5*(sAoA - this->sin_stall_angle) + 1.0);
+            output.CL = frac*this->CL_max;
         }
     }
     else
     {
-        CL = this->CL_alpha*AoA;
-        CD = CD0_compressible + this->K*CL*CL;
+        output.CL = this->CL_alpha*sAoA;
+        output.CD += this->K*output.CL*output.CL;
     }
+
+    return output;
+}
+
+void SingleStageAerodynamics::update()
+{
+    // Get aerodynamic quantities needed for computation
+    this->compute_aero_values();
+
+    this->force.zero();
+    this->moment.zero();
+
+    // if airspeed too low don't compute, (has divide by zero consequences anyway)
+    if(this->aero_values.airspeed < 1e-2)
+    {
+        return;
+    }
+
+    // already compute damping moment
+    this->moment += rocket.state.angular_velocity*(this->CM_alpha_dot*this->aero_values.dynamic_pressure);
+
+    // arm is the moment arm formed from the freestream, length of the arm is sin of angle between
+    Vector arm = this->aero_values.unit_v_air.cross(rocket.state.CS.axis.z);
+
+    // sin of the angle of attack
+    double sAoA = arm.norm();
+
+    auto coef = this->get_aero_coef(sAoA);
+
+    Vector drag = this->aero_values.unit_v_air*(coef.CD*this->aero_values.dynamic_pressure);
+
+    this->force -= drag;
+
+    if(sAoA < AOA_THRESHOLD)
+    {
+        return;
+    }
+
+    // normalize arm
+    arm *= (1.0/sAoA);
 
     Vector lift = arm.cross(this->aero_values.unit_v_air);
-    double v = lift.norm();
-    if(v > 1e-8) {
-        CL /= v; //
-    }
 
-    this->force = (lift*CL - this->aero_values.unit_v_air*CD)*this->aero_values.dynamic_pressure;
+    this->moment -= arm*(coef.CM*this->aero_values.dynamic_pressure); // TODO: check orientations
+
+    lift *= coef.CL/lift.norm();
+
+    this->force += lift;
 }
