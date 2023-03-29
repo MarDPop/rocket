08ef1ba273fd94dcc8a0e2fd8bac068f7db08c26
Marius <marius.popescu20@gmail.com>
2023-03-29 07:27:52 -0600

    wip

--- src/proto/src/Servo.cpp
+++ src/proto/src/Servo.cpp
@@ -6,7 +6,20 @@ void Servo::update(double torque, double time)
     {
         double dt = time - this->time_ref;
 
-        double angle
+        double angle_to_move = this->commanded_angle - this->angle;
+
+        double angle_movement_possible = this->slew_rate*dt;
+
+        if(angle_movement_possible > fabs(angle_to_move))
+        {
+            angle_movement_possible = angle_to_move;
+        }
+        else
+        {
+            angle_movement_possible *= (angle_to_move > 0);
+        }
+
+        this->angle += angle_movement_possible;
     }
 
     this->time_ref = time;
